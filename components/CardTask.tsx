2ab002d7fb5edde4141c3634b243b14968d00d68
GustavoTavaresDosSantos <yaantavaresdossantos2003@gmail.com>
2025-06-24 15:07:57 -0300

    Alterações em Home.tsx e criado components/CardTask.tsx
    Adicionei um exemplo do FlatList no Home.tsx e criei um CardTask para mostrar ele no Home (isso também já ajudando quando forem criar o AddTask.

---
+++ components/CardTask.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CardTask({ title, description }: any) {
+  return (
+    <View>
+      <Text>CardTask</Text>
+    </View>
+  );
+}
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,3 +1,21 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
 export default function Home() {
-  return "Home";
+  return (
+    <View style={styles.container}>
+      {/* <FlatList
+        data={[]}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={(item) => item.id}
+      /> */}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+});
