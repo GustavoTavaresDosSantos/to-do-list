f7f4b52f4f60f219cfc384147445230f629e7e7e
GustavoTavaresDosSantos <yaantavaresdossantos2003@gmail.com>
2025-06-24 17:09:39 -0300

    V0.0.4 GT
    Finalizado programação e estilização básica (provavelmente vamos mudar depois) do CardTask e Home.tsx, além de adicionar no App.tsx para testar (ainda não testado), assim finalizando a aula de terça-feira (24/06/2025).

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,34 +1,105 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  ListRenderItem,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 import CardTask from "../components/CardTask";
 
-const tarefas = [
-  {
-    id: "1",
-    title: "When I met you in the summer",
-    description: "To my heart beat sound",
-    done: "",
-  },
-  {
-    id: "2",
-    title: "Na faixa de gaza",
-    description: "Só homem bomba, na guerra é tudo ou nada",
-    done: "",
-  },
-];
+interface Tarefa {
+  id: string;
+  title: string;
+  description: string;
+  done: boolean;
+}
 
-export default function Home() {
+export default function Home({ navigation }: { navigation: any }) {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([
+    {
+      id: "1",
+      title: "When I met you in the summer",
+      description: "To my heart beat sound",
+      done: false,
+    },
+    {
+      id: "2",
+      title: "Na faixa de gaza",
+      description: "Só homem bomba, na guerra é tudo ou nada",
+      done: false,
+    },
+  ]);
+
   const [selecionadas, setSelecionadas] = useState<string[]>([]);
+
+  function toggleSelect(id: string) {
+    if (selecionadas.includes(id)) {
+      setSelecionadas(selecionadas.filter((item) => item !== id));
+    } else {
+      setSelecionadas([...selecionadas, id]);
+    }
+  }
+
+  function selecionarTudo() {
+    if (selecionadas.length === tarefas.length) {
+      setSelecionadas([]);
+    } else {
+      setSelecionadas(tarefas.map((item) => item.id));
+    }
+  }
+
+  function apagarSelecionadas() {
+    setTarefas(tarefas.filter((item) => !selecionadas.includes(item.id)));
+    setSelecionadas([]);
+  }
+
+  function marcarComoConcluidas() {
+    setTarefas(
+      tarefas.map((item) =>
+        selecionadas.includes(item.id) ? { ...item, done: true } : item
+      )
+    );
+    setSelecionadas([]);
+  }
+
+  const renderItem: ListRenderItem<Tarefa> = ({ item }) => (
+    <CardTask
+      title={item.title}
+      description={item.description}
+      isSelected={selecionadas.includes(item.id)}
+      done={item.done}
+      onSelect={() => toggleSelect(item.id)}
+    />
+  );
 
   return (
     <View style={styles.container}>
+      <View style={styles.defaultHeader}>
+        <Text style={styles.headerTitle}>Minhas Tarefas</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("AddTask")}>
+          <Feather name="plus" style={styles.icon}></Feather>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.selectionHeader}>
+        <TouchableOpacity onPress={selecionarTudo}>
+          <Feather name="square" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={marcarComoConcluidas}>
+          <Feather name="check" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={apagarSelecionadas}>
+          <Feather name="trash" style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+
       <FlatList
         data={tarefas}
-        renderItem={({ item }) => (
-          <CardTask title={item.title} description={item.description} />
-        )}
         keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
@@ -42,5 +113,29 @@ const styles = StyleSheet.create({
   },
   separator: {
     height: 12,
+  },
+  defaultHeader: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  selectionHeader: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  icon: {
+    fontSize: 24,
+  },
+  listContent: {
+    flexGrow: 1,
+    paddingTop: 10,
+    paddingBottom: 100,
   },
 });
