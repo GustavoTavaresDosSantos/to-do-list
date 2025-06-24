ff9d113580beb5bc15cd45ae8dad3378f1c93b8c
GustavoTavaresDosSantos <yaantavaresdossantos2003@gmail.com>
2025-06-24 16:04:24 -0300

    V0.0.2 GT
    Adicionado integração entre CardTask.tsx e Home.tsx

--- components/CardTask.tsx
+++ components/CardTask.tsx
@@ -1,9 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function CardTask({ title, description }: any) {
+type CardTaskProps = {
+  title: string;
+  description: string;
+};
+
+export default function CardTask({ title, description }: CardTaskProps) {
   return (
-    <View>
-      <Text>CardTask</Text>
+    <View style={styles.cardTask}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cardTask: {
+    elevation: 2,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+  },
+});
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,35 @@
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import CardTask from "../components/CardTask";
+import { useState } from "react";
+
+const tarefas = [
+  {
+    id: "1",
+    title: "When I met you in the summer",
+    description: "To my heart beat sound",
+    done: "",
+  },
+  {
+    id: "2",
+    title: "Na faixa de gaza",
+    description: "Só homem bomba, na guerra é tudo ou nada",
+    done: "",
+  },
+];
 
 export default function Home() {
+  const [selecionadas, setSelecionadas] = useState<string[]>([]);
+
   return (
     <View style={styles.container}>
-      {/* <FlatList
-        data={[]}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+      <FlatList
+        data={tarefas}
+        renderItem={({ item }) => (
+          <CardTask title={item.title} description={item.description} />
+        )}
         keyExtractor={(item) => item.id}
-      /> */}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </View>
   );
 }
@@ -17,5 +39,8 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#fff",
+  },
+  separator: {
+    height: 12,
   },
 });
